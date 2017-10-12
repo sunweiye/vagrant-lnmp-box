70f256c1e7ec7bff77f370101d894c188a1cc00a
Weiye Sun <sun@punkt.de>
2017-10-12 14:19:38 +0200

    Add box configruation examples and enable IP settings in configuration

--- providers/virtualbox.rb
+++ providers/virtualbox.rb
@@ -1,5 +1,5 @@
 def config_vm(vm, config)
-  vm.network 'private_network', ip: '192.168.100.100'
+  vm.network 'private_network', ip: config['vm']['ipv4']
   vm.provider "virtualbox" do |vb|
     vb.memory = config['vm']['memory'] ? config['vm']['memory'] : "2048"
     vb.cpus = config['vm']['cpu_count'] ? config['vm']['cpu_count'] : "1"
